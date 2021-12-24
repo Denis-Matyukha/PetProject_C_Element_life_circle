f670a9be6e45575f8adae0049e1d7a5385b31e96
Denis Matyukha <denmatuhawork@gmail.com>
2021-12-24 17:23:10 +0300

    initial project's condition - ts must be added

---
+++ src/components/template_1/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import bk from './assets/bk_initial.jpg';
+import './style.module.css';
+
+const Template1 = () => {
+  return (
+    <div>
+      <p>look, it works! ^=^</p>
+      <img src={bk} alt="" />
+    </div>
+  );
+};
+
+export default Template1;
